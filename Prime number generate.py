2b1b5b438b20b1273a173efa9a61b384f5468b83
TweakyMeade <antnymeade@gmail.com>
2020-10-13 17:24:58 +0100

    Create Prime number generate.py

---
+++ Prime number generate.py
@@ -0,0 +1,19 @@
+priCand = 1
+primeFlag = True
+while primeFlag:
+    counter = 0
+    primeFlagTwo = True
+    priCand += 1
+    dvd = priCand
+    while primeFlagTwo:
+        if dvd > 0:
+            priPro = priCand % dvd
+        if priPro == 0:
+            counter += 1
+        dvd -= 1
+        if dvd == 0:
+            primeFlagTwo = False
+    if counter <= 2:
+        print(priCand)
+    if priCand == 0:
+        primeFlag = False
